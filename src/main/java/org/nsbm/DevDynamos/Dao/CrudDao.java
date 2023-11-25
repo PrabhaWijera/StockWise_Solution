70cfed821b180ef4635df5a49c63b8d9217ccade
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-11-25 23:53:22 +0530

    developing ......

--- src/main/java/org/nsbm/DevDynamos/Dao/CrudDao.java
+++ src/main/java/org/nsbm/DevDynamos/Dao/CrudDao.java
@@ -1,4 +1,27 @@
 package org.nsbm.DevDynamos.Dao;
 
-public interface CrudDao {
+import org.nsbm.DevDynamos.Dao.exception.ConstraintViolationException;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CrudDao <T extends Serializable,ID extends Serializable> extends SuperDao{
+
+    T save(T entity) throws ConstraintViolationException, SQLException,ClassNotFoundException;
+
+    boolean update(T entity) throws ConstraintViolationException, SQLException,ClassNotFoundException;
+
+    boolean deleteByPk(String pk)throws ConstraintViolationException, SQLException,ClassNotFoundException;
+
+    List<T> findAll();
+
+    T findByPk(ID pk) throws SQLException,ClassNotFoundException;
+
+    boolean existByPk(String pk);
+
+    long count();
+
+
+
 }
