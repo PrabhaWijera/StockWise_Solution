a628604cd0c05116a8c988e9ff38c5867ec91480
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-11-25 23:49:34 +0530

    DbConnection.java

--- src/main/java/org/nsbm/DevDynamos/Db/DbConnection.java
+++ src/main/java/org/nsbm/DevDynamos/Db/DbConnection.java
@@ -1,4 +1,26 @@
 package org.nsbm.DevDynamos.Db;
 
+import org.nsbm.DevDynamos.Dao.util.DButil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+
+    private static DbConnection dbConnection;
+
+    private Connection connection;
+
+    private DbConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection= DriverManager.getConnection("jdbc:mysql://locahost:3306/StockeWiseSolutions?createDatabaseIfNotExist=true","root","1234");
+    }
+    public static DbConnection getInstance() throws SQLException, ClassNotFoundException {
+        return dbConnection==null?new DbConnection():dbConnection;
+    }
+
+    public Connection connection(){
+        return connection;
+    }
 }
